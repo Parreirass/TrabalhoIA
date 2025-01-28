6f148796d22b03e4aa652ca7664a0c2af034c369
Felipe Parreiras <felipeparreiras86@gmail.com>
2025-01-28 09:28:33 -0300

    mudanças

--- SVM/svm.py
+++ SVM/svm.py
@@ -7,7 +7,7 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 # Carregar o arquivo CSV
-caminho_do_csv = '../KNN/winequality-red.csv'
+caminho_do_csv = 'KNN/winequality-red.csv'
 
 # Lê o arquivo CSV e armazena em um DataFrame
 data = pd.read_csv(caminho_do_csv)
@@ -17,7 +17,7 @@ X = data.iloc[:, :-1].values  # Todas as colunas, exceto a última
 y = data.iloc[:, -1].values   # A última coluna
 
 # Dividir os dados em conjuntos de treinamento e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Padronizar os dados (muito importante para PCA e SVM)
 scaler = StandardScaler()
@@ -25,12 +25,12 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Aplicar PCA para reduzir a dimensionalidade
-pca = PCA(n_components=10)  # Reduzindo para 2 componentes principais
+pca = PCA(n_components=5)  # Reduzindo para 2 componentes principais
 X_train_pca = pca.fit_transform(X_train)
 X_test_pca = pca.transform(X_test)
 
 # Treinar o modelo SVM
-svm = SVC(kernel='linear')  # Você pode escolher outros kernels como 'rbf'
+svm = SVC(kernel='poly')  # Você pode escolher outros kernels como 'rbf'
 svm.fit(X_train_pca, y_train)
 
 # Fazer previsões no conjunto de teste
